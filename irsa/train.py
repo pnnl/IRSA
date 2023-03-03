f12c7260d1a93d860e9d9f7ecda708912f885acf
Sean Colby <sean.colby@pnnl.gov>
2023-03-03 08:54:16 -0800

    Add train loop

--- irsa/train.py
+++ irsa/train.py
@@ -1,53 +1,114 @@
+from collections import OrderedDict
+
 import torch
+import torch.optim as optim
+from torch import nn
 
 
-def train(model, train_loader, val_loader, num_epochs, criterion):
+def train(model, train_loader, val_loader, num_epochs):
+    # Select device
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # Initialize network
+    print(model)
+
+    # Send to device
+    model = model.to(device)
+
+    # Optimizer
+    optimizer = optim.Adam(model.parameters(), lr=1E-3)
+
+    # Loss function
+    loss_fn = nn.BCEWithLogitsLoss()
+
+    # Control initialization
+    best_val_loss = float("Inf")
+
+    # Loss history containers
     train_losses = []
     val_losses = []
-    cur_step = 0
 
+    # Iterate epochs
     for epoch in range(num_epochs):
-        running_loss = 0.0
+        # Model in train mode
         model.train()
 
-        print("Starting epoch " + str(epoch+1))
-        for spec1, spec2, labels in train_loader:
+        # Training accumulators
+        running_train_loss = 0.0
+        running_train_acc = 0.0
 
-            # Forward
-            spec1 = spec1.to(device)
-            spec2 = spec2.to(device)
-            labels = labels.to(device)
-            outputs = model(spec1, spec2)
-            loss = criterion(outputs, labels)
+        # Iterate over batches
+        for batch in train_loader:
+            # Send to device
+            batch = OrderedDict([(key, value.to(device))
+                                for key, value in batch.items() if '_label' not in key])
+
+            # Predict
+            outputs = model(batch['exp_spec'], batch['pred_spec'])
+
+            # Calculate loss
+            loss = loss_fn(outputs, batch['label'])
+            running_train_loss += loss.item()
+
+            # Calculate accuracy
+            acc = (batch['label'] == (outputs > 0.5)
+                   ).sum().item() / len(outputs)
+            running_train_acc += acc
 
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            running_loss += loss.item()
 
-        avg_train_loss = running_loss / len(train_loader)
-        train_losses.append(avg_train_loss)
-        val_running_loss = 0.0
-
-        # check validation loss after every epoch
+        # Validation loop
         with torch.no_grad():
+            # Model in evaluation mode
             model.eval()
 
-            for spec1, spec2, labels in val_loader:
-                spec1 = spec1.to(device)
-                spec2 = spec2.to(device)
-                labels = labels.to(device)
-                outputs = model(spec1, spec2)
-                loss = criterion(outputs, labels)
-                val_running_loss += loss.item()
+            # Validation accumulators
+            running_val_loss = 0.0
+            running_val_acc = 0.0
 
-        avg_val_loss = val_running_loss / len(val_loader)
+            # Iterate validation instances
+            for batch in val_loader:
+                # Send to device
+                batch = OrderedDict(
+                    [(key, value.to(device)) for key, value in batch.items() if '_label' not in key])
+
+                # Predict
+                outputs = model(batch['exp_spec'], batch['pred_spec'])
+
+                # Calculate loss
+                loss = loss_fn(outputs, batch['label'])
+                running_val_loss += loss.item()
+
+                # Calculate accuracy
+                acc = (batch['label'] == (outputs > 0.5)).sum().item()
+                running_val_acc += acc / len(outputs)
+
+        # Average training metrics
+        avg_train_loss = running_train_loss / len(train_loader)
+        avg_train_acc = running_train_acc / len(train_loader)
+
+        # Keep training loss
+        train_losses.append(avg_train_loss)
+
+        # Average validation metrics
+        avg_val_loss = running_val_loss / len(val_loader)
+        avg_val_acc = running_val_acc / len(val_loader)
+
+        # Keep track of validation loss
         val_losses.append(avg_val_loss)
 
-        pstring = 'Epoch [{}/{}], Train Loss: {:.4f}, Valid Loss: {:.8f}'
-        print(pstring.format(epoch+1, num_epochs, avg_train_loss, avg_val_loss))
+        # Save best weights
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            torch.save(model.state_dict(), 'model/ir_exp_pred_pnn.pt')
+            print('**Epoch [{}/{}] Train Loss: {:.4f}, Val Loss: {:.4f}, Train Acc: {:.4f}, Val Acc: {:.4f}**'
+                  .format(epoch + 1, num_epochs, avg_train_loss, avg_val_loss, avg_train_acc, avg_val_acc))
+        else:
+            print('Epoch [{}/{}] Train Loss: {:.4f}, Val Loss: {:.4f}, Train Acc: {:.4f}, Val Acc: {:.4f}'
+                  .format(epoch + 1, num_epochs, avg_train_loss, avg_val_loss, avg_train_acc, avg_val_acc))
 
     print("Finished Training")
     return train_losses, val_losses
- 
